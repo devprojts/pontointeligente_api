16fb010ef841f5396b372ee9f8efc57948019f09
devprojts <devprojts@gmail.com>
2020-01-27 12:09:44 -0300

    profile teste hiraki no pom.xml

--- src/test/java/com/devprojects/pontointeligente/api/PontoInteligenteApplicationTests.java
+++ src/test/java/com/devprojects/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -2,12 +2,13 @@ package com.devprojects.pontointeligente.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class PontoInteligenteApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
