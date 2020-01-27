e1198bc54d61197d21351d6803d369c6add84c1e
devprojts <devprojts@gmail.com>
2020-01-27 12:55:16 -0300

    removendo notacao test da classe de teste

--- src/test/java/com/devprojects/pontointeligente/api/PontoInteligenteApplicationTests.java
+++ src/test/java/com/devprojects/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -2,9 +2,9 @@ package com.devprojects.pontointeligente.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
+
+
 @SpringBootTest
 class PontoInteligenteApplicationTests {
 
